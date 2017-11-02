d9b759ecdbb78fd558b95a9bb32d6cfffc935119
Jason Turner <jason@wirelessprv-10-193-47-197.near.illinois.edu>
2017-11-02 13:47:38 -0500

    Adding Viscous Burgers Script. In Development...

---
+++ ViscousBurgers.py
@@ -0,0 +1,9 @@
+# Viscous Burgers Equation
+# Jason Turner - Fall 2017
+#
+# NOTE :
+# To do: 
+#______________________________________________________________________________#
+
+import numpy as np
+import matplotlib.pyplot as plt
